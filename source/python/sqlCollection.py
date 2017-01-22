fdb3754152bcc08e41f5ff371d0bbf37364d4df4
Nick <ultrasound2k@gmail.com>
2017-01-22 21:18:27 +0900

    sqlcollection.py file is for createDB sql, insertDB sql, selectDB sql

--- source/python/sqlCollection.py
+++ source/python/sqlCollection.py
@@ -1,54 +1,58 @@
-import obd
 import sqlite3
 
-conn = sqlite3.connect('test.db')
-c = conn.cursor()
-# t = ('RHAT',)
-c.execute('SELECT * FROM tb1')
-print(c.fetchone())
-# Save (commit) the changes
-# conn.commit()
-conn.close()
+dbname = 'obd.db'
 
-connection = obd.OBD() # auto-connects to USB or RF port
-cmd = obd.commands.SPEED # select an OBD command (sensor)
+def tablecreate():
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
+    try:
+        cursor.execute("""CREATE TABLE rowOBDdata (
+                             rpm INTEGER PRIMARY KEY UNIQUE NOT NULL,
+                             speed INTEGER NOT NULL,
+                             maf INTEGER NOT NULL,
+                             timedata DATE NOT NULL)""")
 
+    except sqlite3.Error as e:
+        print('Failed to creat table:', e)
 
-response = connection.query(cmd) # send the command, and parse the response
-print(response.value) # returns unit-bearing values thanks to Pint
-print(response.value.to("mph")) # user-friendly unit conversions
+    connection.commit()
+    connection.close()
 
+    return None
 
+def tableselect():
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
 
-#sqlite> .save FILE
+    try:
+        results = cursor.execute("SELECT * FROM rowOBDdata")
+        response = []
+        for row in results.fetchall():
+            response.append(row)
 
-#
-# obd.OBD            # main OBD connection class
-# obd.Async          # asynchronous OBD connection class
-# obd.commands       # command tables
-# obd.Unit           # unit tables (a Pint UnitRegistry)
-# obd.OBDStatus      # enum for connection status
-# obd.scan_serial    # util function for manually scanning for OBD adapters
-# obd.OBDCommand     # class for making your own OBD Commands
-# obd.ECU            # enum for marking which ECU a command should listen to
-# obd.logger         # the OBD module's root logger (for debug)
+    except sqlite3.Error as e:
+        print('Failed to select data:', e)
 
+    connection.commit()
+    connection.close()
 
-# from obd import OBDCommand, Unit
-# from obd.protocols import ECU
-# from obd.utils import bytes_to_int
-#
-# def rpm(messages):
-#     """ decoder for RPM messages """
-#     d = messages[0].data
-#     v = bytes_to_int(d) / 4.0  # helper function for converting byte arrays to ints
-#     return v * Unit.RPM # construct a Pint Quantity
-#
-# c = OBDCommand("RPM", \          # name
-#                "Engine RPM", \   # description
-#                b"010C", \        # command
-#                2, \              # number of return bytes to expect
-#                rpm, \            # decoding function
-#                ECU.ENGINE, \     # (optional) ECU filter
-#                True)             # (optional) allow a "01" to be added for speed
+    return (response)
 
+def tableinsert(rpm, speed, maf, timedata):
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
+    rpm = rpm
+    speed = speed
+    maf = maf
+    timedata = timedata
+
+    try:
+        cursor.execute("INSERT INTO rowOBDdata (rpm, speed, maf, timedata) VALUES (?, ?, ?, ?)", (rpm, speed, maf, timedata))
+
+    except sqlite3.Error as e:
+        print('Failed to insert data:', e)
+
+    connection.commit()
+    connection.close()
+
+    return None
