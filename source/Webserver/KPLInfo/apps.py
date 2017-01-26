dd84e9c1c2532292da6d9f77539d582eeae33d12
banghyun89 <jamse88@nate.com>
2017-01-26 17:51:58 +0900

    DataManage(main.py, readOBD.py, saveOBD.py, sqlCollection.py)
    WebServer/KPLInfo/templates/KPLInfo/  : menu.html, menu2.html, history.html, live.html

---
+++ source/Webserver/KPLInfo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KplinfoConfig(AppConfig):
+    name = 'KPLInfo'
---
+++ source/Webserver/KPLInfo/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def menu(request):
+    return render(request, 'KPLInfo/menu.html')
+    #return HttpResponse("Hello World")
+
+def live(request):
+    return render(request, 'KPLInfo/live.html')
+
+
+def history(request):
+    return render(request, 'KPLInfo/history.html')
+
+#def requestHistoryData(request):
+
+
+#def requestLiveData(request):
