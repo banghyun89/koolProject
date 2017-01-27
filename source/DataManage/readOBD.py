52280e36583ac8bd4528fb6247c3c6a8272b016c
jihooyim <jihooyim@gmail.com>
2017-01-27 15:26:04 +0900

    commit readObd

--- source/DataManage/readOBD.py
+++ source/DataManage/readOBD.py
@@ -1,26 +1,55 @@
 import obd
-import sqlite3
+import time
+#import saveOBD
 
-conn = sqlite3.connect('test.db')
-c = conn.cursor()
-# t = ('RHAT',)
-c.execute('SELECT * FROM tb1')
-print(c.fetchone())
-# Save (commit) the changes
-# conn.commit()
-conn.close()
+connection = obd.OBD('/dev/pts/4') # auto-connects to USB or RF port
+cmd1 = obd.commands.SPEED   # select an OBD command (sensor)
+cmd2 = obd.commands.MAF     # select an OBD command (sensor)
 
-connection = obd.OBD() # auto-connects to USB or RF port
-cmd = obd.commands.SPEED # select an OBD command (sensor)
+dataList = []
 
 
-response = connection.query(cmd) # send the command, and parse the response
-print(response.value) # returns unit-bearing values thanks to Pint
-print(response.value.to("mph")) # user-friendly unit conversions
+while 1:
+    try:
+        vss = connection.query(cmd1) # send the command, and parse the response
+        maf = connection.query(cmd2)
 
+        if (vss is None) or (maf is None):
+            continue
 
+    #   print(vss)
+    #   print(maf)
 
-#sqlite> .save FILE
+        currentTime = time.strftime("%Y%m%d_%H:%M:%S",time.localtime(vss.time))
+#        print(currentTime)
+        dataList.append(currentTime)
+        dataList.append(vss.value.magnitude)
+        dataList.append(maf.value.magnitude)
+
+    #   print(vss.value.magnitude)
+    #   print(maf.value.magnitude)
+        kpl = 30.215 * ((vss.value.magnitude) / (maf.value.magnitude))
+
+        dataList.append(kpl)
+
+        print(dataList)
+        print('')
+
+        """
+        insertDB(dataList)     DB insert 부분
+        """
+
+        dataList = []
+
+    #    print(1/kpl)
+    #    valStr = str(kpl)
+    #    print(valStr + " kpl")
+        time.sleep(1)
+
+    except IOError as e:
+        print(e)
+
+#sqlite> .save FILE   ee
 
 #
 # obd.OBD            # main OBD connection class
@@ -51,4 +80,3 @@ print(response.value.to("mph")) # user-friendly unit conversions
 #                rpm, \            # decoding function
 #                ECU.ENGINE, \     # (optional) ECU filter
 #                True)             # (optional) allow a "01" to be added for speed
-
