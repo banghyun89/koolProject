63b140175f102c39618e67be8bd0b450f807babf
jihooyim <jihooyim@gmail.com>
2017-01-29 01:15:18 +0900

    commit Webserver~~~~~

--- source/Webserver/KPLInfo/views.py
+++ source/Webserver/KPLInfo/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 from django.http import JsonResponse
-#from .models import Obd
 from customPackage import sqlCollection
 from datetime import datetime, timedelta
 
