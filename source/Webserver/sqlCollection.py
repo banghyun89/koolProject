ad56ec5eaad7f88dbfacddb555df81925e6b5494
jihooyim <jihooyim@gmail.com>
2017-01-30 21:57:01 +0900

    commit Change webpages.

--- source/Webserver/sqlCollection.py
+++ source/Webserver/sqlCollection.py
@@ -45,18 +45,34 @@ def put_data(obd_data):
     return True
 
 
-def get_history_data(time_cond_list):
+def get_history_data(list):
     connection = sqlite3.connect(dbname)
     cursor = connection.cursor()
     try:
         data = []
-        for i in time_cond_list:
-            data.append(i)
-        results = cursor.execute("SELECT time, vss, maf, kpl FROM rowOBDdata WHERE time >= ? AND time <= ?",(data[0], data[1]))
-        #results = cursor.execute("SELECT time, vss, maf, kpl FROM rowOBDdata WHERE time >= '2017-01-27' AND time <= '2017-01-27'")
         response = []
-        for row in results.fetchall():
-            response.append(row)
+        response2 = []
+        selectList = []
+        count = 0
+        for i in list:
+            data.append(i)
+        if data[2] > 0 :
+            results = cursor.execute("SELECT id, time, vss, maf, kpl FROM rowOBDdata WHERE time >= ? AND time < ? and id < ? ORDER BY id DESC LIMIT ?, ?",(data[0], data[1], data[2], data[3], data[4]))
+            for row in results.fetchall():
+                selectList.append(row)
+        else :
+            results = cursor.execute(
+                "SELECT id, time, vss, maf, kpl FROM rowOBDdata WHERE time >= ? AND time < ? ORDER BY id DESC LIMIT ?, ?",(data[0], data[1], data[3], data[4]))
+            for row in results.fetchall():
+                selectList.append(row)
+
+            countList = cursor.execute(
+                "SELECT count(*) FROM rowOBDdata WHERE time >= ? AND time < ?",(data[0], data[1]))
+            for row in countList.fetchall():
+                response2.append(row)
+            if len(response2) > 0 :
+                count = response2[0]
+
 
     except sqlite3.Error as e:
         print('Failed to select data:', e)
@@ -64,15 +80,16 @@ def get_history_data(time_cond_list):
     connection.commit()
     connection.close()
 
+    response.append(selectList)
+    response.append(count)
     return (response)
-
 
 def get_live_data():
     connection = sqlite3.connect(dbname)
     cursor = connection.cursor()
 
     try:
-        results = cursor.execute("SELECT kpl FROM rowOBDdata ORDER BY id DESC LIMIT 1")
+        results = cursor.execute("SELECT time,kpl,vss,maf FROM rowOBDdata ORDER BY id DESC LIMIT 1")
         respone = []
         for row in results.fetchall():
             respone.append(row)
