135ad975302564c99895310b5a5190477aa93e0c
jihooyim <jihooyim@gmail.com>
2017-01-30 21:57:01 +0900

    commit Change webpages.

--- source/Webserver/KPLInfo/views.py
+++ source/Webserver/KPLInfo/views.py
@@ -9,28 +9,41 @@ def menu(request):
     return render(request, 'KPLInfo/menu.html')
 
 def getLiveData(request):
-
-    rs = sqlCollection.get_live_data()
-    return JsonResponse({'realFuelValue': rs, 'avgFuelValue':rs})
+    rsList = sqlCollection.get_live_data()
+    return JsonResponse({'list': rsList})
 
 def getHistoryData(request):
     v = int(request.GET.get('searchPeriod'))
+    rowCnt  = int(request.GET.get('rowCnt'))
+    callCnt  = int(request.GET.get('callCnt'))
+    firstId = int(request.GET.get('firstId'))
+    fromCnt = callCnt * rowCnt
+    toCnt = rowCnt
 
     now = datetime.now()
     timegap = timedelta(days=v)
     before = now - timegap
-    todateGap = timedelta(days=1)
+    todateGap = timedelta(days=2)
     after = now + todateGap
 
     if v==1:
         fromDate = now.strftime('%Y-%m-%d')
     else :
         fromDate = before.strftime('%Y-%m-%d')
+
     toDate = after.strftime('%Y-%m-%d')
+
     param = []
     param.append(fromDate)
     param.append(toDate)
+    param.append(firstId)
+    param.append(fromCnt)
+    param.append(toCnt)
+
+    print('param')
+    print(param)
     rsList = sqlCollection.get_history_data(param)
+
 
     return JsonResponse({'list': rsList})
 
--- source/Webserver/readOBD_random.py
+++ source/Webserver/readOBD_random.py
@@ -4,14 +4,33 @@ import random
 #import sqlCollection
 from customPackage import sqlCollection
 
-connection = obd.OBD('/dev/cu.OBDKeyPro-DevB-1') # auto-connects to USB or RF port
-cmd1 = obd.commands.SPEED   # select an OBD command (sensor) Unit.kph ; km/h range : 0~1000
-cmd2 = obd.commands.MAF     # select an OBD command (sensor) Mass Air Flow g/s
+#connection = obd.OBD('/dev/cu.OBDKeyPro-DevB-1') # auto-connects to USB or RF port
+#cmd1 = obd.commands.SPEED   # select an OBD command (sensor) Unit.kph ; km/h range : 0~1000
+#cmd2 = obd.commands.MAF     # select an OBD command (sensor) Mass Air Flow g/s
 
 cmd1 = 'cmd1'
 cmd2 = 'cmd2'
 
 dataList = []
+
+class valueObj:
+    def __init__(self, number):
+        self.magnitude = number
+
+class cmdObj:
+    def __init__(self, number):
+        self.time = time.time()
+        self.value = valueObj(number)
+
+
+class obdObj:
+    def query(self,v):
+        if v == 'cmd1' :
+            return cmdObj(round(random.uniform(0, 200.99), 2)) #vss
+        else :
+            return cmdObj(round(random.uniform(0, 99.99), 2)) #maf
+
+connection = obdObj()
 
 while 1:
     try:
