e208b0a72d2986396b092b2c9d014913f32f85e3
jihooyim <jihooyim@gmail.com>
2017-02-02 15:49:44 +0900

    commit
    add d3
    add css file
    add graph source for reserching

--- kool/web/kool/views.py
+++ kool/web/kool/views.py
@@ -29,3 +29,26 @@ def live(request):
 
 def history(request):
     return render(request, 'templates/history.html')
+
+def live_graph(request):
+    return render(request, 'templates/live_graph.html')
+
+
+def getLiveData_graph(request):
+    rsList = sqlCollection.get_live_data()
+    return JsonResponse({'list': rsList})
+
+def history_graph(request):
+    return render(request, 'templates/history_graph.html')
+
+def getHistoryData_graph(request):
+    param = []
+    param.append(request.GET.get('fromDate'))
+    param.append(request.GET.get('toDate'))
+    param.append(int(request.GET.get('latestIndex')))
+    param.append(int(request.GET.get('fromRow')))
+    param.append(int(request.GET.get('rowLimitCount')))
+    print(param)
+    rsList = sqlCollection.get_history_data(param)
+
+    return JsonResponse({'list': rsList[0], 'totalCount': rsList[1]})
